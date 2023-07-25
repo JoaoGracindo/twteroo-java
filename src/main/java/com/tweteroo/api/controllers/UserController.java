fab3295fb7d94cae6a1a3c72e25cc64b55a3c5dd
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-07-25 17:44:07 -0300

    Feat: Post user route

--- src/main/java/com/tweteroo/api/controllers/UserController.java
+++ src/main/java/com/tweteroo/api/controllers/UserController.java
@@ -1,16 +1,23 @@
 package com.tweteroo.api.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tweteroo.api.models.UserModel;
+import com.tweteroo.api.repositories.UserRepository;
+
 @RestController
 @RequestMapping("/sign-up")
 public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
     
     @PostMapping
-    public void create(@RequestBody String userInfo){
-        System.out.println("foi?");
+    public void create(@RequestBody UserModel userInfo){
+        this.userRepository.save(userInfo);
     }
 }
