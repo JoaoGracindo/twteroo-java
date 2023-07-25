c6eb5b2d53a22146c57b1afda08731cb341d7f9b
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-07-25 17:53:00 -0300

    Feat: tweet model

---
+++ src/main/java/com/tweteroo/api/models/TweetModel.java
@@ -0,0 +1,56 @@
+package com.tweteroo.api.models;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tweets")
+public class TweetModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String username;
+
+    private String avatar;
+
+    private String text;
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAvatar() {
+        return this.avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
