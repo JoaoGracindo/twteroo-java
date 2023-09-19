fecc0c5065b8b7b52b8696205677cbb3fbea891e
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-09-19 18:53:15 -0300

    Feat: get tweets by page

--- src/main/java/com/tweteroo/api/controllers/TweetsController.java
+++ src/main/java/com/tweteroo/api/controllers/TweetsController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tweteroo.api.models.TweetModel;
@@ -23,7 +24,9 @@ public class TweetsController {
     }
 
     @GetMapping
-    public java.util.List<TweetModel> find(){
-        return this.tweetRepository.findAll();
+    public java.util.List<TweetModel> find(@RequestParam("page") Integer page){
+        Integer start = 0 + (5 * (page - 1));
+        Integer end = 4 + (5 * (page - 1));
+        return this.tweetRepository.findAll().subList(start, end);
     }
 }
