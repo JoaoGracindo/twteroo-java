b65b7e6a05bfab01745421895c726f985ab1b080
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-09-19 22:21:27 -0300

    Feat: find tweets by user

--- src/main/java/com/tweteroo/api/controllers/TweetsController.java
+++ src/main/java/com/tweteroo/api/controllers/TweetsController.java
@@ -3,6 +3,7 @@ package com.tweteroo.api.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,18 @@ public class TweetsController {
     @Autowired
     UserRepository userRepository;
 
+    @GetMapping
+    public java.util.List<TweetModel> find(@RequestParam("page") Integer page){
+        Integer start = 0 + (5 * (page - 1));
+        Integer end = 4 + (5 * (page - 1));
+        return this.tweetRepository.findAll().subList(start, end);
+    }
+
+    @GetMapping("/{username}")
+    public java.util.List<TweetModel> findByUser(@PathVariable String username){
+        return this.tweetRepository.findAllByUsername(username);
+    }
+
     @PostMapping
     public void create(@RequestBody TweetModel tweetInfo){
         String avatar = this.userRepository.findByUsername(tweetInfo.getUsername()).get(0).getAvatar();
@@ -30,10 +43,4 @@ public class TweetsController {
         this.tweetRepository.save(tweetInfo);
     }
 
-    @GetMapping
-    public java.util.List<TweetModel> find(@RequestParam("page") Integer page){
-        Integer start = 0 + (5 * (page - 1));
-        Integer end = 4 + (5 * (page - 1));
-        return this.tweetRepository.findAll().subList(start, end);
-    }
 }
